b15b6ca7d739ffc4e211fb30dc1250bb0ff543e6
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-01-23 16:18:24 -0600

    The tests for the hooks that list entities were added

--- test/hooks.spec.ts
+++ test/hooks.spec.ts
@@ -3,7 +3,7 @@ import express from "express";
 import assert from "assert";
 
 import { ControllerMethods, Middleware, ResultEntity, ROCKET_RESULT } from "../src";
-import { serviceHandler } from "../src/hooks";
+import { serviceHandler, Hook } from "../src/hooks";
 import { Unauthorized } from "../src/errors";
 
 const app = express();
@@ -20,7 +20,7 @@ const items: Partial<ResultEntity>[] = [
     dir: "images"
   },
   {
-    name: "filesrocket-local",
+    name: "filesrocket-local.png",
     size: 1234567890,
     dir: "images"
   }
@@ -36,7 +36,7 @@ class Controller implements ControllerMethods {
 
   list(): Middleware {
     return (req, _, next) => {
-      req = Object.defineProperty(req, ROCKET_RESULT, { value: items });
+      req = Object.defineProperty(req, ROCKET_RESULT, { value: items.slice() });
       next();
     }
   }
@@ -71,11 +71,11 @@ describe("Hooks for entity creation", () => {
     }
   }));
 
-  it("Unauthorized", (done) => {
+  it("Before: Unauthorized", (done) => {
     request(app).post("/files").expect(401, done);
   });
 
-  it("Assing username to data", (done) => {
+  it("After: Assing username to data", (done) => {
     request(app)
     .post("/files")
     .set("authorization", "Bearer aklxbwqiy28")
@@ -88,15 +88,49 @@ describe("Hooks for entity creation", () => {
   });
 });
 
-describe("List hook", () => {
-  // app.get("/files", serviceHandler({
-  //   controller: new Controller(),
-  //   method: "list",
-  //   hooks: {
-  //     before: [],
-  //     after: []
-  //   }
-  // }));
+describe("Hooks to list entities", () => {
+  const pushFile: Hook = (req, _, next) => {
+    (req as any)[ROCKET_RESULT].push({
+      name: "filesrocket-cloudinary.png",
+      size: 1234567890,
+      dir: "images"
+    });
+
+    next();
+  }
+
+  const checkAccessToken: Hook = (req, _, next) => {
+    const { access_token } = req.query;
+    if (!access_token) return next(new Unauthorized("You need access_token"));
+    return next();
+  }
+
+  app.get("/files", serviceHandler({
+    controller: new Controller(),
+    method: "list",
+    hooks: {
+      before: [checkAccessToken],
+      after: [pushFile]
+    }
+  }));
+
+  it("Before: Unautorized", (done) => {
+    request(app)
+      .get("/files")
+      .expect(401, done)
+  });
+
+  it("After: Enter an entity in items", (done) => {
+    request(app)
+      .get("/files")
+      .query({access_token: "kxabweiu"})
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.ok(res.body.length > items.length)
+        done();
+      });
+  });
 });
 
 describe("Remove hook", () => {
