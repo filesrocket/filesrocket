26adef4900cd2d418788f30aca0c802de4345898
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-03-29 18:39:39 -0600

    Tests were added to the service.spec.ts file

--- test/service.spec.ts
+++ test/service.spec.ts
@@ -1,6 +1,7 @@
 import assert from 'assert'
 
-import { Filesrocket } from '../src/service'
+import { FileController } from '../src/controllers/file.controller'
+import { Filesrocket } from '../src/index'
 
 const filesrocket = new Filesrocket()
 
@@ -16,5 +17,32 @@ describe('Filesrocket service', () => {
   it('Check the existence of services', () => {
     assert.ok(filesrocket.services.length > 1)
     assert.ok(filesrocket.services.length === 2)
+  })
+
+  it('Get a service', () => {
+    const service = filesrocket.service('serviceOne')
+    assert.ok(typeof service !== 'undefined')
+    assert.ok(service instanceof OneService)
+  })
+
+  it('Get a controller', () => {
+    const controller = filesrocket.controller('serviceOne')
+    assert.ok(typeof controller !== 'undefined')
+    assert.ok(controller instanceof FileController)
+  })
+
+  it('Get a service list', () => {
+    const items = filesrocket.services
+    assert.ok(items.length === 2)
+  })
+
+  it('Get a service that does not exist', () => {
+    const service = filesrocket.controller('random')
+    assert.ok(typeof service === 'undefined')
+  })
+
+  it('Get a controller that does not exist', () => {
+    const controller = filesrocket.controller('random')
+    assert.ok(typeof controller === 'undefined')
   })
 })
