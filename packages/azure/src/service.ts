88d0bfe555b5ecf4660d2977b25ced19c551dded
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-09-28 19:54:08 -0600

    feat(azure/service): Added method to get single file

--- packages/azure/src/service.ts
+++ packages/azure/src/service.ts
@@ -1,5 +1,6 @@
 import { InputEntity, OutputEntity, Paginated, Query, ServiceMethods } from '@filesrocket/core'
 import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob'
+import path from 'path'
 
 import { Options } from './declarations'
 
@@ -34,11 +35,46 @@ export class AzureService implements ServiceMethods {
   }
 
   async list (query?: Query | undefined): Promise<Paginated<OutputEntity> | OutputEntity[]> {
-    throw new Error('Method not implemented.')
+    const { containerName } = this.options
+
+    const containerClient = this.blobServiceClient.getContainerClient(query?.containerName || containerName)
+
+    const iterator = containerClient.listBlobsFlat().byPage({ maxPageSize: query?.size })
+
+    const res = (await iterator.next()).value
+
+    console.log(res)
+
+    console.log('block blob client: ', await containerClient.getBlockBlobClient('Avril_Lavigne_-_Ums_click2music-63ezffsl8dy08bi.jpg').getProperties())
+
+    return res as any
   }
 
-  async get (id: string, query?: Query | undefined): Promise<OutputEntity> {
-    throw new Error('Method not implemented.')
+  async get (id: string, query: Query = {}): Promise<OutputEntity> {
+    const containerName = query.containerName || this.options.containerName
+
+    const container = this.blobServiceClient.getContainerClient(containerName)
+
+    const blob = container.getBlockBlobClient(id)
+
+    const res = await blob.getProperties()
+
+    const chunks = id.split('/')
+    const dir = chunks.slice(0, chunks.length - 1).join('/')
+    const name = chunks.at(-1) as string
+
+    const { ext } = path.parse(id)
+
+    return {
+      id,
+      name,
+      dir,
+      ext,
+      url: res._response.request.url as string,
+      size: res.contentLength as number,
+      createdAt: res.createdOn as Date,
+      updatedAt: res.lastModified as Date
+    }
   }
 
   async remove (id: string, query?: Query | undefined): Promise<OutputEntity> {
